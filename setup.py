108a077b8921c72188142c13d9c91ba2194d39b3
Ivan Lee <ivanklee86@gmail.com>
2018-10-02 18:10:02 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Setup file for unleash-client"""
+from setuptools import setup, find_packages
+
+
+def readme():
+    """Include README.rst content in PyPi build information"""
+    with open('README.md') as file:
+        return file.read()
+
+setup(
+    name='unleash-client',
+    version='0.0.1',
+    description='Python client for the wonderful unleash feature flag framework!',
+    long_description=readme(),
+    url='https://github.com/ivanklee86/unleash-client',
+    author='Ivan Lee',
+    author_email='ivanklee86@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+        ],
+    tests_require=['pytest'],
+    zip_safe=False,
+    include_package_data=True
+)
+
