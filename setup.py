3ee6afc78dc0ac0fb25fd83f9221b46b6293710b
Ivan Lee <ivanklee86@gmail.com>
2019-04-23 04:58:18 -0400

    v2.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='UnleashClient',
-    version='2.2.0',
+    version='2.2.1',
     author='Ivan Lee',
     author_email='ivanklee86@gmail.com',
     description='Python client for the Unleash feature toggle system!',
