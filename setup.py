432c87c99078e7dc7f4f9193fa211e16ab2e07b2
Ivan Lee <ivanklee86@gmail.com>
2019-11-02 08:06:36 -0400

    Automatic version bump via bumpversion.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
     name='UnleashClient',
-    version='3.2.0',
+    version='3.3.0',
     author='Ivan Lee',
     author_email='ivanklee86@gmail.com',
     description='Python client for the Unleash feature toggle system!',
