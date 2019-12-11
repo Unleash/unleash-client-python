c584a29e06e4608bbd40e0c78415944b2d0e70d1
Ivan Lee <ivanklee86@gmail.com>
2019-12-11 01:12:33 -0500

    Update Unleash Hosted integration test to include custom strategy.

--- tests/integration_tests/integration_unleashhosted.py
+++ tests/integration_tests/integration_unleashhosted.py
@@ -1,5 +1,25 @@
 import time
 from UnleashClient import UnleashClient
+from UnleashClient.strategies import Strategy
+
+# ---
+class DogTest(Strategy):
+    def load_provisioning(self) -> list:
+        return [x.strip() for x in self.parameters["sound"].split(',')]
+
+    def apply(self, context: dict = None) -> bool:
+        """
+        Turn on if I'm a dog.
+
+        :return:
+        """
+        default_value = False
+
+        if "sound" in context.keys():
+            default_value = context["sound"] in self.parsed_provisioning
+
+        return default_value
+
 
 # ---
 import logging
@@ -15,11 +35,16 @@ handler.setFormatter(formatter)
 root.addHandler(handler)
 # ---
 
+custom_strategies_dict = {
+        "amIADog": DogTest
+    }
+
 my_client = UnleashClient(
     url="https://eu.unleash-hosted.com/demo/api",
     environment="staging",
     app_name="pyIvan",
-    custom_headers={'Authorization': '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d'}
+    custom_headers={'Authorization': '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d'},
+    custom_strategies=custom_strategies_dict
 )
 
 my_client.initialize_client()
@@ -27,7 +52,9 @@ my_client.initialize_client()
 while True:
     time.sleep(10)
     context = {
-        'userId': "1"
+        'userId': "1",
+        'sound': 'woof'
     }
-    print(my_client.is_enabled("ivantest", context))
-    print(my_client.get_variant("ivan-variations", context))
+    print(f"ivantest: {my_client.is_enabled('ivantest', context)}")
+    print(f"ivan-variations: {my_client.get_variant('ivan-variations', context)}")
+    print(f"ivan-customstrategyx: {my_client.is_enabled('ivan-customstrategy', context)}")
