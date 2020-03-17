a4f7e33346eb6e07da74b4e693d448ce9ef4ec5a
Ivan Lee <ivanklee86@gmail.com>
2020-03-17 05:44:07 -0400

    Update integration tests & add heroku script.

--- tests/integration_tests/integration_unleashhosted.py
+++ tests/integration_tests/integration_unleashhosted.py
@@ -40,7 +40,7 @@ custom_strategies_dict = {
     }
 
 my_client = UnleashClient(
-    url="https://eu.unleash-hosted.com/demo/api",
+    url="https://app.unleash-hosted.com/demo/api",
     environment="staging",
     app_name="pyIvan",
     custom_headers={'Authorization': '56907a2fa53c1d16101d509a10b78e36190b0f918d9f122d'},
