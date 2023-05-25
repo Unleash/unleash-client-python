220931edc8e07091b1f7e94a1c4ddcd6d9065b4e
Thomas Heartman <thomas@getunleash.io>
2023-05-25 12:39:39 +0200

    Tests: fix

--- tests/unit_tests/test_features.py
+++ tests/unit_tests/test_features.py
@@ -81,20 +81,20 @@ def test_select_variation_variation(test_feature_variants):
 def test_variant_metrics_with_existing_variant(test_feature_variants):
     for iteration in range(1, 7):
         test_feature_variants.get_variant({"userId": "2"})
-        assert test_feature_variants.variants_counts["VarB"] == iteration
+        assert test_feature_variants.variant_counts["VarB"] == iteration
 
 
 def test_variant_metrics_with_disabled_feature(test_feature_variants):
     assert not test_feature_variants.is_enabled()
     for iteration in range(1, 7):
         test_feature_variants.get_variant()
-        assert test_feature_variants.variants_counts["disabled"] == iteration
+        assert test_feature_variants.variant_counts["disabled"] == iteration
 
 
-def test_variant_metrics_feature_has_no_variants(test_feature_variants):
+def test_variant_metrics_feature_has_no_variants(test_feature):
     for iteration in range(1, 7):
         test_feature.get_variant()
-        assert test_feature.variants_counts["disabled"] == iteration
+        assert test_feature.variant_counts["disabled"] == iteration
 
 
 ## remaining test cases:
