2797e25c43b94ba5f2a0a5e88479980b6bc4594d
Fredrik Oseberg <fredrik.no@gmail.com>
2023-11-29 11:29:48 +0100

    refactor: add tests for gradual rollout discrepancy

--- tests/unit_tests/test_client.py
+++ tests/unit_tests/test_client.py
@@ -234,6 +234,24 @@ def test_uc_is_enabled(unleash_client):
     time.sleep(1)
     assert unleash_client.is_enabled("testFlag")
 
+@responses.activate
+def test_consistent_results(unleash_client):
+    responses.add(responses.POST, URL + REGISTER_URL, json={}, status=202)
+    responses.add(
+        responses.GET, URL + FEATURES_URL, json=MOCK_FEATURE_RESPONSE, status=200
+    )
+    responses.add(responses.POST, URL + METRICS_URL, json={}, status=202)
+    unleash_client.initialize_client()
+
+    results = [unleash_client.is_enabled("testFlag2") for i in range(1000)]
+    true_count = results.count(True)
+    false_count = results.count(False)
+    
+    # Due to murmur hash variations on smaller datasets, we allow a 10% discrepancy
+    discrepancy = 100  # 10% of 1000
+    assert 500 - discrepancy <= true_count <= 500 + discrepancy, "True count is outside acceptable range"
+    assert 500 - discrepancy <= false_count <= 500 + discrepancy, "False count is outside acceptable range"
+
 
 @responses.activate
 def test_uc_project(unleash_client_project):
@@ -846,3 +864,4 @@ def test_signals_feature_flag(cache):
     assert unleash_client.is_enabled("testFlag")
     variant = unleash_client.get_variant("testVariations", context={"userId": "2"})
     assert variant["name"] == "VarA"
+
