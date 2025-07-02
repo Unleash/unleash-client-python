80d845e1ca15a0da35c5156377d06b73e288ad80
Simon Hornby <liquidwicked64@gmail.com>
2025-07-02 09:26:17 +0200

    chore: add new event types

--- UnleashClient/events.py
+++ UnleashClient/events.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
 from uuid import UUID
+from json import loads
 
 
 class UnleashEventType(Enum):
@@ -11,17 +12,51 @@ class UnleashEventType(Enum):
 
     FEATURE_FLAG = "feature_flag"
     VARIANT = "variant"
+    FETCHED = "fetched"
+    READY = "ready"
 
 
 @dataclass
-class UnleashEvent:
+class BaseEvent:
     """
-    Dataclass capturing information from an Unleash feature flag or variant check.
+    Base event type for all events in the Unleash client.
     """
 
     event_type: UnleashEventType
     event_id: UUID
+
+
+@dataclass
+class UnleashEvent(BaseEvent):
+    """
+    Dataclass capturing information from an Unleash feature flag or variant check.
+    """
+
     context: dict
     enabled: bool
     feature_name: str
     variant: Optional[str] = ""
+
+
+@dataclass
+class UnleashReadyEvent(BaseEvent):
+    """
+    Event indicating that the Unleash client is ready.
+    """
+
+    pass
+
+
+@dataclass
+class UnleashFetchedEvent(BaseEvent):
+    """
+    Event indicating that the Unleash client has fetched feature flags.
+    """
+
+    raw_features: str
+
+    @property
+    def features(self) -> dict:
+        if not hasattr(self, "_parsed_payload"):
+            self._parsed_payload = loads(self.raw_features)
+        return self._parsed_payload
